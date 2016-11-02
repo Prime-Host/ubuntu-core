b9929996269d6c3d824c268190abfc5b77b74158
root <root@vmd13810.contabo.host>
2016-11-02 23:00:29 +0100

    delte not used stuff

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Add system user for Wordpress
 
-useradd -m -d /home/wordpress -p $(openssl passwd -1 'wordpress') -G root -s /bin/bash wordpress \
-    && usermod -a -G www-data wordpress \
-    && usermod -a -G sudo wordpress \
-    && ln -s /usr/share/nginx/www /home/wordpress/www
+useradd -m -d /home/$USER -G root -s /bin/bash $USER \
+	&& usermod -a -G $USER $USER \
+	&& usermod -a -G sudo $USER
 
-    WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-    #This is so the passwords show up in logs.
-    echo "wordpress:$SSH_PASSWORD" | chpasswd
-    echo "root:$SSH_PASSWORD" | chpasswd
+# Set passwords for the ssh user and root
+echo "$USER:$SSH_PASSWORD" | chpasswd
+echo "root:$SSH_PASSWORD" | chpasswd
 
 
 # start all the services
