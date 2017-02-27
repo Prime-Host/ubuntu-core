6529e503b93a15d64dc06030e44fa39f9fda0595
Nurian <gregorius.stahlbart@gmail.com>
2017-02-27 17:23:42 +0100

    password variable for multiple containers

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create custom ssh_user with sudo privileges
-useradd -m -d /home/$SSH_USER -G root -s /bin/bash $SSH_USER \
-	&& usermod -a -G $SSH_USER $SSH_USER \
-	&& usermod -a -G sudo $SSH_USER
+useradd -m -d /home/$PRIMEHOST_USER -G root -s /bin/bash $PRIMEHOST_USER \
+	&& usermod -a -G $PRIMEHOST_USER $PRIMEHOST_USER \
+	&& usermod -a -G sudo $PRIMEHOST_USER
 
 # Set passwords for the ssh_user and root
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-echo "root:$SSH_PASSWORD" | chpasswd
+echo "$PRIMEHOST_USER:$PRIMEHOST_PASSWORD" | chpasswd
+echo "root:$PRIMEHOST_PASSWORD" | chpasswd
 
 
 # start all the services
